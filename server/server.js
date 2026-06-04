58577625fe6983adf2f177ecd0168ed6abbcbd48
Raz3-33 <raziq@ocelots.in>
2026-06-04 15:35:10 +0530

    Fix server.js issue

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import next from "next";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
-import path from "path";
 
-import connect from "./connect/connect.js";
+
+
+import connect from "./connect/connect.js"
 import { errorHandler, notFound } from "./middlewares/errorMiddlware.js";
 import productRouter from "./routes/productRouter.js";
 import shopProductRouter from "./routes/shopProductRouter.js";
@@ -15,27 +15,22 @@ import cmsRouter from "./routes/cmsRouter.js";
 import authRouter from "./routes/authRouter.js";
 import sanitizedConfig from "./config.js";
 
-let dev = sanitizedConfig.NODE_ENV === "production";
-const nextApp = next({
-  dev,
-  dir: path.join(process.cwd(), "../client"),
-});
-const handle = nextApp.getRequestHandler();
-
 dotenv.config();
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  }),
-);
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+}));
+
 
 // Middleware
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
 
 // db Connectig
 connect()
@@ -44,6 +39,7 @@ connect()
     console.error("Database connection failed:", err.message);
     process.exit(1);
   });
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/shop/products", shopProductRouter);
@@ -54,41 +50,12 @@ app.use("/api/admin/cms/", cmsRouter);
 // user managment router
 app.use("/api/admin/userManagment/", userManagmentRouter);
 
-// let dirname = path.resolve();
-
-// if (sanitizedConfig.NODE_ENV === "production") {
-//   try {
-//     app.use(express.static(path.join(dirname, "../client", ".next")));
-
-//     app.use((req, res) => {
-//       res.sendFile(path.resolve(dirname, "../client", ".next", "index.html"));
-//     });
-//   } catch (error) {
-//     console.log(
-//       error,
-//       "errorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerror"
-//     );
-//   }
-// }
-
-// app.get("/", (req, res) => {
-//   res.send("API is running!");
-// });
-
-nextApp.prepare().then(() => {
-  app.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  app.use(notFound);
-  app.use(errorHandler);
-
-  const PORT = sanitizedConfig.PORT || 8000;
-
-  app.listen(process.env.PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.get("/", (req, res) => {
+  res.send("API is running!");
 });
 
-// const PORT = sanitizedConfig.PORT || 8000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = sanitizedConfig.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
